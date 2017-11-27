d761771ab12b58647b91ea318df7ed6179790541
Alex Parvanov <aparvanov@abv.bg>
2017-11-27 09:48:57 +0200

    moving some folders

---
+++ Entrance exams/Oct11_Task3/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oct11_Task3
+{
+    class Program
+    {
+        static void Main()
+        {
+            var sequence = Console.ReadLine().Split(' ').Select(long.Parse).ToArray();
+            var differenceArray = new long[sequence.Length - 1];
+            for (int i = 1; i <= sequence.Length - 1; i++)
+            {
+                long smallerNum = 0;
+                long biggerNum = 0;
+                if (sequence[i - 1] >= sequence[i])
+                {
+                    biggerNum = sequence[i - 1];
+                    smallerNum = sequence[i];
+                }
+                else
+                {
+
+                    smallerNum = sequence[i - 1];
+                    biggerNum = sequence[i];
+                }
+                long difference = biggerNum - smallerNum;
+                differenceArray[i - 1] = difference;
+            }
+
+            for (int j = 1; j < sequence.Length; j++)
+            {
+                long difference = sequence[j] - sequence[j - 1];
+                if (difference % 2 == 0)
+                {
+                    if (j < differenceArray.Length)
+                    {
+                        differenceArray[j] = 0;
+                    }
+                    j++;
+                }
+            }
+
+            long sumForPrint = differenceArray.Where(n => n % 2 == 0).Sum();
+            Console.WriteLine(sumForPrint);
+        }
+    }
+}
---
+++ Entrance exams/Task2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<int> primes = new List<int>();
+
+            if (n <= 2)
+            {
+                primes.Add(1);
+                primes.Add(2);
+            }
+            else
+            {
+                primes.Add(1);
+                primes.Add(2);
+                for (int i = 3; i <= n; i++)
+                {
+                    bool isPrime = ToTestPrimeNum(i);
+                    if (isPrime)
+                    {
+                        primes.Add(i);
+                    }
+                }
+            }
+            
+            foreach (var primenum in primes.OrderBy(p => p).ToList())
+            {
+                for (int i = 1; i <= primenum; i++)
+                {
+                    bool isPrimeForPrint = ToTestPrimeNum(i);
+                    if (isPrimeForPrint)
+                    {
+                        Console.Write(1);
+                    }
+                    else
+                    {
+                        Console.Write(0);
+                    }
+                }
+                Console.WriteLine();
+            }
+
+        }
+
+      
+        static bool ToTestPrimeNum(int num)
+        {
+            
+            for (int i = 2, maxValue = (int)Math.Sqrt(num); i <= maxValue; i++)
+            {
+                if (num % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}

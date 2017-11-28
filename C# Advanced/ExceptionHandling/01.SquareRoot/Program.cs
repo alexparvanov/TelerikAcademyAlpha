f2ee0464f36c529ecb0faff145047a1a97667259
Alex Parvanov <aparvanov@abv.bg>
2017-11-28 13:28:19 +0200

    Adding some files

---
+++ C# Advanced/ExceptionHandling/01.SquareRoot/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            double number = double.Parse(Console.ReadLine());
+
+            if (number < 0)
+            {
+                throw new Exception();
+            }
+            double squareRoot = Math.Sqrt(number);
+            Console.WriteLine("{0:f3}", squareRoot);
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Invalid number");
+        }
+        finally
+        {
+            Console.WriteLine("Good bye");
+        }
+    }
+}
---
+++ C# Advanced/ExceptionHandling/02.EnterNumbers/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static int previousNum = int.MinValue;
+
+    static void Main()
+    {
+        List<int> numsToPrint = new List<int>();
+
+        numsToPrint.Add(1);
+        for (int i = 0; i < 10; i++)
+        {
+            try
+            {
+                ReadNumber(1, 100, numsToPrint);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Exception");
+                return;
+            }
+        }
+
+        numsToPrint.Add(100);
+        Console.WriteLine(string.Join(" < ", numsToPrint));
+    }
+
+    static void ReadNumber(int start, int end, List<int> numsToPrint)
+    {
+        int currentNum = int.Parse(Console.ReadLine());
+        if (currentNum > start && currentNum < end && currentNum > previousNum)
+        {
+            numsToPrint.Add(currentNum);
+            previousNum = currentNum;
+        }
+        else
+        {
+            throw new Exception("Exception");
+        }
+    }
+}
